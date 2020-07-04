decfc5719023443b095160d4639d2fe74c2b6a02
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-07-04 20:08:25 -0300

    chore: change name and input on rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/dice.ts',
+  input: 'src/dice-3d.ts',
   output: {
     dir: 'dist',
     format: 'umd',
-    name: 'dice',
+    name: 'Dice3D',
   },
   plugins: [typescript()],
 };
