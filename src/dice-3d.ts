af85b3ebd159640931dc255ce970e9ffb0a2335c
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-07-05 15:15:35 -0300

    feat: methods to create dice - add method to create dots - add method to create sides of dice - add method to create dice - add method to randomize number

--- src/dice-3d.ts
+++ src/dice-3d.ts
@@ -1,19 +1,54 @@
 interface Options {
   bgColor?: string;
+  button?: string;
   name?: string;
   sides?: number;
 }
 
-export default function Dice3D({
-  bgColor = '#fff',
-  name = 'dice3d-container',
-  sides = 6,
-}: Options) {
-  const render = () => {
-    console.info(bgColor);
-    console.info(name);
-    console.info(sides);
+export default function Dice3D({ sides = 6 }: Options) {
+  const _createDots = (item: HTMLLIElement, indexItem: number) => {
+    let index = 1;
+    for (index; index <= indexItem; index++) {
+      const dot = document.createElement('span');
+      dot.classList.add('dot');
+      item.appendChild(dot);
+    }
+
+    return item;
   };
 
-  render();
+  const _createSides = (list: HTMLUListElement) => {
+    let index = 1;
+
+    for (index; index <= sides; index++) {
+      const item: HTMLLIElement = document.createElement('li');
+      item.classList.add('item');
+      item.setAttribute('data-side', `${index}`);
+      _createDots(item, index);
+      list.appendChild(item);
+    }
+
+    return list;
+  };
+
+  const _createDice = () => {
+    const diceContainer: HTMLDivElement = document.createElement('div');
+    const list: HTMLUListElement = document.createElement('ul');
+
+    diceContainer.classList.add('dice');
+    list.classList.add('dice3d-list', 'even-roll');
+
+    list.dataset.roll = _getRandomNumber().toString();
+    _createSides(list);
+    diceContainer.appendChild(list);
+    document.body.appendChild(diceContainer);
+  };
+
+  const _getRandomNumber = (min = 1, max = 6) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  _createDice();
 }
