0b46ebdfaac3c64632bba6b55ef7191c01aaac09
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-08-05 19:26:05 -0300

    Start the initial tests (#14)
    * chore: add babel config
    
    * chore: install babel and types dependencies
    
    * chojre: update jest config
    
    * test: add initial tests of component

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,15 @@
 module.exports = {
-  jest: {
-    collectCoverageFrom: [
-      "src/**/*.{js,jsx,ts,tsx}",
-      "!<rootDir>/node_modules/",
-    ],
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80,
-      },
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
-    coverageReporters: ["text"],
-    snapshotSerializers: ["my-serializer-module"],
   },
+  coverageReporters: ['text'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
 };
